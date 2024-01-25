f96bf589630f1d552a6de1c39c16fb07d51d3565
mohamed <kone35811@gmail.com>
2024-01-25 15:49:25 +0000

    create seeder for the table entreprise and status

---
+++ database/factories/EntrepriseFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Entreprise>
+ */
+class EntrepriseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'logo' => $this->faker->text,
+            'statut_id' => $this->faker->numberBetween(1,3)
+        ];
+
+    }
+}
---
+++ database/seeders/StatutsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Statut;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StatutsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Statut::create([
+            'name' => 'EURL',
+        ]);
+
+        \App\Models\Statut::create([
+            'name' => 'SNC',
+        ]);
+
+        \App\Models\Statut::create([
+            'name' => 'SA',
+        ]);
+
+    }
+}
