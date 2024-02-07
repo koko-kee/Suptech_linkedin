c57b69fce8eb4bd34a4bdc63bd9ebe3bd9de89b8
Coumba Amidou BA <bcoumba221@gmail.com>
2024-02-07 18:24:13 +0000

    View de la configuration profil

--- resources/views/candidats/profile/profil.blade.php
+++ resources/views/candidats/profile/profil.blade.php
@@ -1,6 +1,22 @@
 @extends('partials._layout')
 
 @section('content')
- 
+
+<div class="card">
+    <div class="card-header">
+        Mon profil
+    </div>
+    <div class="card-body">
+        <h1  class="card-title">Informations Personnelles</h1>
+
+
+        <h6 class="card-title">Nom </h6>
+        <h6 class="card-title">Adresse</h6>
+        <h6 class="card-title">Date de naissance</h6>
+        <h6 class="card-title">Email</h6>
         
+        <a href="#" class="btn btn-primary">Modifier profil</a>
+    </div>
+</div>
+
 @endsection
