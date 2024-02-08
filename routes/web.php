6b6a65f4c2adec7459f20511fce9c303a462be27
Fardat Chanrani <fadatchaanrani@gmail.com>
2024-02-08 14:21:33 +0000

    on a ajoute 2 bouttons

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('auth.login');
+});
+Route::get('/register', function () {
+    return view('auth.register');
+});
+Route::get('/welcome',function(){
+    return view('auth.loginWelcome');
 });
