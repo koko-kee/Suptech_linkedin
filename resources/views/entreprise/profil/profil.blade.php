b0af183f344a4d495da40176349fcd4139e7a4cd
Claye Malounga <clayemalounga@gmail.com>
2024-02-08 17:17:20 +0100

    add files and folders

---
+++ resources/views/entreprise/profil/profil.blade.php
@@ -0,0 +1,80 @@
+@extends('partials._layout')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+                <div class="card">
+                  <img src="{{asset('orange.png')}}" class="" width="px" alt="...">
+                </div>
+                <div class="text-secondary fs-4">
+                    <p>Claye RSX</p>
+                </div>
+                <div>
+                   <a href="{{url('entreprise/profil/edit/{9}')}}" class="btn btn-primary w-100 p-1 fs-4 mb-4 rounded-2 fw-bold">Editer</a>
+                </div>
+        </div>
+        <div class="col-md-7">
+            <div class="bg-primary w-100 py-14 fs-4 mb-4 rounded-2">
+                <h3 class=" offset-4 text-white">Liste des offres</h3>
+            </div>
+            
+            <div class="row mt-4">
+                <div class="col-md-4">
+                    <div class="card p-2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum non, libero nam quas at
+                            que perferendis iure quisquam debitis maior
+                            es maxime?
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum non, libero nam quas at
+                            que perferendis iure quisquam debitis maior
+                            es maxime?
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum non, libero nam quas at
+                            que perferendis iure quisquam debitis maior
+                            es maxime?
+                        </p>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card p-2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum non, libero nam quas at
+                            que perferendis iure quisquam debitis maior
+                            es maxime?
+                        </p>
+                    </div>
+                </div> 
+                <div class="col-4 ">
+                    <div class="card p-2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum non, libero nam quas at
+                            que perferendis iure quisquam debitis maior
+                            es maxime?
+                        </p>
+                    </div>
+                </div>
+                <div class="col-4 ">
+                    <div class="card p-2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum non, libero nam quas at
+                            que perferendis iure quisquam debitis maior
+                            es maxime?
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="text-center">
+            <p>Statistiques des differentes candidatures durant l'année</p>
+        </div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Entreprise\ProfilController;
+use App\Http\Controllers\Entreprise\OffreController;
 use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('/entreprise', function () {
+    return view('entreprise.index');
+});
+
+Route::get('/entreprise/profil', [ProfilController::class,'index'])->name('entreprise.profil');
+Route::get('/entreprise/profil/edit/{id}', [ProfilController::class,'edit'])->name('entreprise.profil.edit/{id}');
+Route::get('/entreprise/offre', [OffreController::class,'create'])->name('entreprise.offre');
+Route::post('/entreprise/offre', [OffreController::class,'store'])->name('entreprise.offre.store');
