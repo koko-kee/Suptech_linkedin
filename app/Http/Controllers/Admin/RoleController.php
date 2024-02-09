35aa1d35f4b68c7087612199ebbef9a76bea36ca
Fardat Chanrani <fadatchaanrani@gmail.com>
2024-02-09 17:05:18 +0000

    Gestion roles

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -3,16 +3,51 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
     public function index()
     {
-       // return view('roles.index');
+        $roles = Role::all();
+        return view('roles.index',compact('roles'));
     }
-    public function create()
+    public function store(Request $request)
     {
-        return view('roles.create');
+        $request->validate([
+            "name"=>[
+                "required",
+                
+            ],
+            "description"=>["nullable"]
+        ]);
+        $role = new Role();
+        $role->name = $request->name;
+        $role->description = $request->description;
+        $role->save();
+        return redirect()->route('roles.index');
+
     }
+    public function edit(Request $request,$id)
+    {
+        $role = Role::find($id);
+        return view('roles.edit',compact('role'));
+    }
+    public function update(Request $request,$id)
+    {
+        $role = Role::find($id);
+        $role->name = $request->name;
+        $role->description = $request->description;
+        $role->update();
+        return redirect()->route('roles.index');
+    }
+    public function destroy(Request $request,$id)
+    {
+        $role = Role::find($id);
+        $role->delete();
+        return redirect()->route('roles.index');
+
+    }
+   
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 namespace  App\Http\Controllers;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ForgetController;
+use App\Http\Controllers\Admin\RoleController;
 use App\Models\Role;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -29,7 +30,11 @@ Route::get('/register/{id}',[RegisterController::class,'registerForm'])->name('r
 Route::post('/register',[RegisterController::class,'store'])->name('register.store');
 Route::get('/createCompany',[RegisterController::class,'FormCreateCompany'])->name('FormCreateCompany');
 Route::post('/createCompany',[RegisterController::class,'createCompany'])->name('createCompany');
-Route::get('/roles',[RoleController::class,'create'])->name('roles.crete');
+Route::get('/roles',[RoleController::class,'index'])->name('roles.index');
+Route::post('/roles/create',[RoleController::class,'store'])->name('roles.store');
+Route::get('/roles/edit/{id}',[RoleController::class,'edit'])->name('roles.edit');
+Route::patch('/roles/update/{id}',[RoleController::class,'update'])->name('roles.update');
+Route::delete('/roles/destroy/{id}',[RoleController::class,'destroy'])->name('roles.destroy');
 
 Route::get('/forgetPassword',[ForgetController::class,'FormForget'])->name('forget');
 Route::post('/resetPassword',[ForgetController::class,'Reset'])->name('reset');
