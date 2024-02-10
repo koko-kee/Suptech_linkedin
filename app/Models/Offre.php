baf9adc03f57934e3a77cf6da40ec795c4f233b5
mohamed <kone35811@gmail.com>
2024-02-10 18:38:45 +0000

    kkkk

--- app/Models/Offre.php
+++ app/Models/Offre.php
@@ -9,10 +9,8 @@ class Offre extends Model
 {
     use HasFactory;
 
-    public  function demandes()
+    public function  demandes()
     {
         return $this->hasMany(Demande::class);
     }
 }
-
-
